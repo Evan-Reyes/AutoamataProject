99c176c4bf49b799cb094d63ca34fc8a8f1d4183
morganmadsen <56410691+morganmadsen@users.noreply.github.com>
2022-12-14 16:22:54 -0600

    Update promptUser.cpp
    Fixing spacing, changing restaurant name

--- promptUser.cpp
+++ promptUser.cpp
@@ -3,13 +3,13 @@
 int promptUser()
 {
 	int choice;
-	cout << "**************************************************"<<endl;
-	cout << "*  Hello! Welcome to **NAME RESTAUARANT HERE** ! *"<<endl;
-	cout << "*  1. Display Food Menu                          *"<<endl;
-	cout << "*  2. Enter a Translation                        *"<<endl;
-	cout << "*  3. Read a Definition                          *"<<endl;
-	cout << "*  4. Exit                                       *"<<endl;
-	cout << "**************************************************"<<endl;
+	cout << "*********************************************8*********"<<endl;
+	cout << "*  Hello! Welcome to Il Miglior Ristorante Italiano!  *"<<endl;
+	cout << "*  1. Display Food Menu                               *"<<endl;
+	cout << "*  2. Enter a Translation                             *"<<endl;
+	cout << "*  3. Read a Definition                               *"<<endl;
+	cout << "*  4. Exit                                            *"<<endl;
+	cout << "*******************************************************"<<endl;
 
 	cout << "What would you like to do? Please enter 1, 2, 3, or 4: ";
 	cin >> choice;
